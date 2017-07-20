c4150f3e1cf7d5a8b608f7a835026b0c463f1d0d
Sofia-Sagayam <mailtoosofi@gmail.com>
2017-07-20 19:33:03 +0530

    step 1 boiler plate 20/07/2017

---
+++ NumericOperation/src/NumberProcessing.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class NumberProcessing {
+
+	public int getInput(){
+		
+		//instantiate the class which is used to get input.
+		//create integer data and assign user input value to it.
+		//handle the error in input and display msg.
+		//return in input integer.
+	}
+	
+	public int[] includeSpace(int num){
+		
+		//check the digit count of given argument.
+		//declare an integer array of size digit count.
+		
+		//using loop split the given argument and initilise the integer array
+		
+		 
+		//return the initilised array.
+	}
+	
+	public int[] sortIt(int arr[]){
+	//calculate the length of array argument.
+//declare an integr variable name it temp which will be used in loop.
+//using loop arrange the argument array in ascending order.
+	//return the sorted array.
+	}
+	
+	public int sumIt(int arr[],String oddrEven){
+		//declare the necessary variables
+		//use switch to find out the type of sum either for odd or even with argument oddrEven.
+		//if case odd --apply logic to find odd numbers from the array argument sumit using loop and store result.
+		//if case even --apply logic to find even numbers from the array argument sumit using loop and store result.
+		
+		//return the result.
+	}
+}
